LineageOS/android_device_google_coral

floral: Import and tailor {extract-files,setup-makefiles}.sh
* Partial pick of Id399faa404207ce018f0f0187c9576b61f5d4dc9.

Change-Id: I866e015e3ba717ae7fc585dc56f118f3e251154c

extract-files.sh
@@ -1,0 +1,72 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=coral
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        --flame )               shift
+                                FLAME_SRC=$1
+                                ;;
+        --coral )               shift
+                                CORAL_SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/coral-proprietary-files.txt "$CORAL_SRC" "$SECTION"
+extract "$MY_DIR"/coral-proprietary-files-vendor.txt "$CORAL_SRC" "$SECTION"
+
+# Reinitialize the helper for flame
+DEVICE=flame
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/flame-proprietary-files.txt "$FLAME_SRC" "$SECTION"
+extract "$MY_DIR"/flame-proprietary-files-vendor.txt "$FLAME_SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=coral
+DEVICE_COMMON=coral
+VENDOR=google
+
+INITIAL_COPYRIGHT_YEAR=2020
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
+
+# Copyright headers and guards
+write_headers "coral flame"
+
+write_makefiles "$MY_DIR"/coral-proprietary-files.txt true
+write_makefiles "$MY_DIR"/coral-proprietary-files-vendor.txt true
+
+cat << EOF >> "$ANDROIDMK"
+EOF
+
+# Finish coral
+write_footers
+
+# Reinitialize the helper
+DEVICE=flame
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/flame-proprietary-files.txt true
+write_makefiles "$MY_DIR"/flame-proprietary-files-vendor.txt true
+
+cat << EOF >> "$ANDROIDMK"
+EOF
+
+# Finish
+write_footers

abalam1980/device_google_coral

extract-files.sh
@@ -1,0 +1,72 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=coral
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+AOSIP_ROOT="$MY_DIR"/../../..
+
+HELPER="$AOSIP_ROOT"/vendor/aosip/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        --flame )               shift
+                                FLAME_SRC=$1
+                                ;;
+        --coral )               shift
+                                CORAL_SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/coral-proprietary-files.txt "$CORAL_SRC" "$SECTION"
+extract "$MY_DIR"/coral-proprietary-files-vendor.txt "$CORAL_SRC" "$SECTION"
+
+# Reinitialize the helper for flame
+DEVICE=flame
+setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/flame-proprietary-files.txt "$FLAME_SRC" "$SECTION"
+extract "$MY_DIR"/flame-proprietary-files-vendor.txt "$FLAME_SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=coral
+DEVICE_COMMON=coral
+VENDOR=google
+
+INITIAL_COPYRIGHT_YEAR=2020
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+AOSIP_ROOT="$MY_DIR"/../../..
+
+HELPER="$AOSIP_ROOT"/vendor/aosip/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT" true
+
+# Copyright headers and guards
+write_headers "coral flame"
+
+write_makefiles "$MY_DIR"/coral-proprietary-files.txt true
+write_makefiles "$MY_DIR"/coral-proprietary-files-vendor.txt true
+
+cat << EOF >> "$ANDROIDMK"
+EOF
+
+# Finish coral
+write_footers
+
+# Reinitialize the helper
+DEVICE=flame
+setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/flame-proprietary-files.txt true
+write_makefiles "$MY_DIR"/flame-proprietary-files-vendor.txt true
+
+cat << EOF >> "$ANDROIDMK"
+EOF
+
+# Finish
+write_footers