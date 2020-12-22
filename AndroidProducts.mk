5ea17f530c3503b10b7d59530464824c2d3d5d0a
abalam1980 <emailbrian.reijnders@gmail.com>
2020-12-22 18:43:35 +0100

    play with some toys

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/gzosp_coral.mk \
-    $(LOCAL_DIR)/gzosp_flame.mk
+    $(LOCAL_DIR)/lineage_coral.mk \
+    $(LOCAL_DIR)/lineage_flame.mk
 
 COMMON_LUNCH_CHOICES := \
-    gzosp_coral-userdebug \
-    gzosp_flame-userdebug
+    lineage_coral-userdebug \
+    lineage_flame-userdebug
